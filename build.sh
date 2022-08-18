78fa91ee728c247fb037fef6391168fca6dd00e5
PRD <32349731+fakeGenuis@users.noreply.github.com>
2022-08-18 16:50:48 +0800

    [fix] permanent mysql passwd

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 host_ip=xxx
-pass_db=$(pwgen -c -n -y -s -1 -r \$\#\,)
 
 [[ -d ./.private  ]]|| mkdir ./.private/
 openssl req -x509 -out ./.private/${host_ip}.crt -keyout ./.private/${host_ip}.key \
@@ -9,19 +8,17 @@ openssl req -x509 -out ./.private/${host_ip}.crt -keyout ./.private/${host_ip}.k
    printf "[dn]\nCN=${host_ip}\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:${host_ip}\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
 envsub () {
-    fi="$1"
     eval "cat <<EOF
-    $(<${fi})
+    $(<$1)
 EOF"
 }
 
 envsub ./templates/Caddyfile > Caddyfile
 envsub ./templates/docker-compose.yaml > docker-compose.yaml
 
-cat > ./.private/db.env <<EOF
+[[ -f ./.private/db.env ]] || cat > ./.private/db.env <<EOF
+MYSQL_ROOT_PASSWORD=$(pwgen -c -n -y -s -1 -r \$\#\,)
 MYSQL_PASSWORD=$(pwgen -c -n -y -s -1 -r \$\#\,)
 MYSQL_DATABASE=nextcloud
 MYSQL_USER=nextcloud
 EOF
-
-git stash save
