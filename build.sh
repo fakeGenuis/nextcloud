5a6eac89cc467f0a0ee9a08f04dbc5c2914b1815
PRD <lishux@mail.ustc.edu.cn>
2022-08-31 17:16:52 +0800

    [add] network interface name

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-host_ip=xxx
+[ -z "$1" ] || host_ip=xxx && host_ip="$1"
 
-[[ -d ./.private  ]]|| mkdir ./.private/
-openssl req -x509 -out ./.private/${host_ip}.crt -keyout ./.private/${host_ip}.key \
+[ -d ./.private ] || mkdir ./.private/
+[ -f ./.private/${host_ip}.key ] || openssl req -x509 -out ./.private/${host_ip}.crt -keyout ./.private/${host_ip}.key \
   -newkey rsa:4096 -nodes -sha256 -days 90\
   -subj '/CN=${host_ip}' -extensions EXT -config <( \
    printf "[dn]\nCN=${host_ip}\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:${host_ip}\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
@@ -16,7 +16,7 @@ EOF"
 envsub ./templates/Caddyfile > Caddyfile
 envsub ./templates/docker-compose.yaml > docker-compose.yaml
 
-[[ -f ./.private/db.env ]] || cat > ./.private/db.env <<EOF
+[ -f ./.private/db.env ] || cat > ./.private/db.env <<EOF
 MYSQL_ROOT_PASSWORD=$(pwgen -c -n -y -s -1 -r \$\#\,)
 MYSQL_PASSWORD=$(pwgen -c -n -y -s -1 -r \$\#\,)
 MYSQL_DATABASE=nextcloud
