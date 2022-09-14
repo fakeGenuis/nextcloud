8e5ba5daa0695268a36a9f95bd519f4f7a01391c
PRD <lishux@mail.ustc.edu.cn>
2022-09-14 17:03:23 +0800

    [add] auto renew certificate if expired

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 [ -z "$1" ] || host_ip=xxx && host_ip="$1"
 
+expired () {
+  ssl-cert-check -c "$1" -x 10 | awk '{print $2}' | tail -1 | cut -c-5
+}
+
 [ -d ./.private ] || mkdir ./.private/
-[ -f ./.private/${host_ip}.key ] || openssl req -x509 -out ./.private/${host_ip}.crt -keyout ./.private/${host_ip}.key \
+key_file=./.private/"${host_ip}".crt
+[ -f "$key_file" ] && [ "$(expired $key_file)" != "Expir" ] || openssl req -x509 -out ./.private/${host_ip}.crt -keyout ./.private/${host_ip}.key \
   -newkey rsa:4096 -nodes -sha256 -days 90\
-  -subj '/CN=${host_ip}' -extensions EXT -config <( \
+  -subj "/CN=${host_ip}" -extensions EXT -config <( \
    printf "[dn]\nCN=${host_ip}\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:${host_ip}\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
 envsub () {
